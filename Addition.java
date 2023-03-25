5d3ccf5924d92c9d8e519fff9aef7bc6d677e780
suraj2434 <128876985+suraj2434@users.noreply.github.com>
2023-03-25 10:45:12 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class  Addition
+{
+	public static void main(String[] args) 
+	{
+		short s=(short) 4444;
+		char c=(char) 123;
+		byte b=(byte) 24;
+
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(b);
+ 
+	}
+}
